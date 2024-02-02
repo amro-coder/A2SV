4296975eea2d961d4f02611e936a942fde06af74
Amro Salman <54499426+amro-coder@users.noreply.github.com>
2024-02-03 00:06:33 +0200

    Create Python: Division.py

---
+++ Python: Division.py
@@ -0,0 +1,7 @@
+# problem link:https://www.hackerrank.com/challenges/python-division/problem?isFullScreen=true
+# solution:
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a//b)
+    print(a/b)
