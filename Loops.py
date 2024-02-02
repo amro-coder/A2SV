ef3fe149da7d710e375c4877277ca17685f39109
Amro Salman <54499426+amro-coder@users.noreply.github.com>
2024-02-03 00:14:54 +0200

    Create Loops.py

---
+++ Loops.py
@@ -0,0 +1,7 @@
+# problem link: https://www.hackerrank.com/challenges/python-loops/problem
+# solution:
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(n):
+        print(i*i)
+
