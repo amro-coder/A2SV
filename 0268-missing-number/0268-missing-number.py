6b2c900f8e9e4e50e0f5efecd60007eb3ad2b5b2
Amro Salman <54499426+amro-coder@users.noreply.github.com>
2024-02-09 20:01:59 +0200

    Time: 102 ms (95.39%), Space: 17.9 MB (64.62%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,10 +1,3 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        n=len(nums)
-        visited=[False]*(n+1)
-        for value in nums:
-            visited[value]=True
-        for value in range(n+1):
-            if (not visited[value]):
-                return value
-        
+        return len(nums)*(len(nums)+1)//2-sum(nums)
