7ab4bfdd92b8500b9cde4120faa396b2885b2c54
Amro Salman <54499426+amro-coder@users.noreply.github.com>
2024-02-09 22:27:17 +0200

    Time: 94 ms (87.65%), Space: 17.9 MB (79.70%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,10 +1,40 @@
 from collections import Counter
 class Solution:
-    def majorityElement(self, nums: List[int]) -> List[int]:
-        count=Counter(nums)
-        ans=[]
-        for key,value in count.items():
-            if (value>len(nums)//3):
-                ans.append(key)
+    def majorityElement(self,nums):
+        first_voted_value = 10**10
+        first_vote_cnt = 0
+        second_voted_value = 10**10
+        second_vote_cnt = 0
+        for i in range(len(nums)):
+            if nums[i]==first_voted_value:
+                first_vote_cnt+=1
+            elif nums[i]==second_voted_value:
+                second_vote_cnt+=1
+            elif first_vote_cnt == 0:
+                first_voted_value = nums[i]
+                first_vote_cnt = 1
+            elif second_vote_cnt==0:
+                second_voted_value = nums[i]
+                second_vote_cnt = 1
+            else:
+                first_vote_cnt-=1
+                second_vote_cnt-=1
+
+        first_vote_cnt = second_vote_cnt = 0
+        for i in range(len(nums)):
+            if nums[i] == first_voted_value:
+                first_vote_cnt += 1
+            if nums[i] == second_voted_value:
+                second_vote_cnt += 1
+
+        ans = []
+        if first_vote_cnt > len(nums) // 3:
+            ans.append(first_voted_value)
+        if second_vote_cnt > len(nums) // 3:
+            ans.append(second_voted_value)
         return ans
+                
+        
+        
+        
         
