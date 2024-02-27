48b853bbd759051debc745fd3aab73d7a06e970a
Amro Salman <54499426+amro-coder@users.noreply.github.com>
2024-02-27 21:06:37 +0200

    Time: 959 ms (5.01%), Space: 17.1 MB (54.43%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -2,18 +2,18 @@ class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
         n=len(names)
         x=[(heights[i],names[i]) for i in range(n)]
-        # # bubble sort
-        # for i in range(n):
-        #     for j in range(i,n):
-        #         if x[i]<x[j]:
-        #             x[i],x[j]=x[j],x[i]
-        # # selection sort
-        # min_index=0
-        # for i in range(n):
-        #     for j in range(min_index,n):
-        #         if x[j]>x[min_index]:
-        #             x[j],x[min_index]=x[min_index],x[j]
-        #     min_index+=1
+        # bubble sort
+        for i in range(n):
+            for j in range(i,n):
+                if x[i]<x[j]:
+                    x[i],x[j]=x[j],x[i]
+        # selection sort
+        min_index=0
+        for i in range(n):
+            for j in range(min_index,n):
+                if x[j]>x[min_index]:
+                    x[j],x[min_index]=x[min_index],x[j]
+            min_index+=1
         # insertion sort
         for last_sorted_index in range(n-1):
             index=last_sorted_index+1
