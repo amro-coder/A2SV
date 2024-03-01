e6bf0d326ddca5f1bf7d4f6637f6ada8fa10ca96
Amro Salman <54499426+amro-coder@users.noreply.github.com>
2024-03-01 15:35:44 +0200

    Time: 3552 ms (7.56%), Space: 17.1 MB (85.29%) - LeetHub

---
+++ 0820-short-encoding-of-words/0820-short-encoding-of-words.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minimumLengthEncoding(self, words: List[str]) -> int:
+        words=sorted(set(words),key=lambda a:len(a))
+        ans=sum([len(i)+1 for i in words])
+        for i in range(len(words)):
+            for j in range(i+1,len(words)):
+                if words[i]==words[j][len(words[j])-len(words[i]):]:
+                    ans-=len(words[i])+1
+                    break
+        return ans
