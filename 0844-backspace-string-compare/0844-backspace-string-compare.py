1220bad4f8f69e4ed076df00d420a88f29a6ce76
Amro Salman <54499426+amro-coder@users.noreply.github.com>
2024-03-04 15:34:28 +0200

    Time: 35 ms (61.22%), Space: 16.5 MB (57.47%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.py
+++ 0844-backspace-string-compare/0844-backspace-string-compare.py
@@ -6,14 +6,16 @@ class Solution:
             if hashtag_cnt==0 and s[i]!="#":
                 new_s.append(s[i])
             else:
-                hashtag_cnt+= (s[i]=="#") - (s[i]!="#")
+                hashtag_cnt+= s[i]=="#"
+                hashtag_cnt-= s[i]!="#"
             
         hashtag_cnt=0
         for i in range(len(t)-1,-1,-1):
             if hashtag_cnt==0 and t[i]!="#":
                 new_t.append(t[i])
             else:
-                hashtag_cnt+= (t[i]=="#") - (t[i]!="#")
+                hashtag_cnt+= t[i]=="#"
+                hashtag_cnt-= t[i]!="#"
 
             
         return new_s==new_t
