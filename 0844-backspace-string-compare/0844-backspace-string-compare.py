a0776057353c73a30a3ad3f17e195dade267eb6c
Amro Salman <54499426+amro-coder@users.noreply.github.com>
2024-03-04 15:27:47 +0200

    Time: 23 ms (99.06%), Space: 16.7 MB (27.67%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.py
+++ 0844-backspace-string-compare/0844-backspace-string-compare.py
@@ -1,16 +1,23 @@
 class Solution:
     def backspaceCompare(self, s: str, t: str) -> bool:
-        s,t=list(s),list(t)
-        while '#' in s:
-            index=s.index('#')
-            if index!=0:
-                s[index-1:index+1]=[]
+        new_s,new_t=[],[]
+        hashtag_cnt=0
+        for i in range(len(s)-1,-1,-1):
+            if hashtag_cnt==0 and s[i]!="#":
+                new_s.append(s[i])
             else:
-                s.pop(index)
-        while '#' in t:
-            index=t.index('#')
-            if index!=0:
-                t[index-1:index+1]=[]
+                hashtag_cnt+= s[i]=="#"
+                hashtag_cnt-= s[i]!="#"
+            
+        hashtag_cnt=0
+        for i in range(len(t)-1,-1,-1):
+            if hashtag_cnt==0 and t[i]!="#":
+                new_t.append(t[i])
             else:
-                t.pop(index)
-        return s==t
+                hashtag_cnt+= t[i]=="#"
+                hashtag_cnt-= t[i]!="#"
+
+            
+        return new_s==new_t
+        
+            
