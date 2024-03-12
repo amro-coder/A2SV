f89aa0db6bb4e9d83ea3e4cde647b403d9575c55
Amro Salman <54499426+amro-coder@users.noreply.github.com>
2024-03-12 13:32:26 +0200

    Time: 51 ms (9.23%), Space: 16.7 MB (25.69%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.py
@@ -0,0 +1,14 @@
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        right_bigger=len(nums)-1
+        for i in range(len(nums)-2,-1,-1):
+            if (nums[i]<nums[right_bigger]):
+                for j in range(len(nums)-1,i,-1):
+                    if nums[j]>nums[i]:
+                        nums[j],nums[i]=nums[i],nums[j]
+                        nums[i+1:]=sorted(nums[i+1:])
+                        break
+                break
+            right_bigger=i
+        else:
+            nums.sort()
