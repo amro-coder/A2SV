f9e40271262d40b9c1bd729b40c5b4079d9cf46d
Amro Salman <54499426+amro-coder@users.noreply.github.com>
2024-03-13 15:58:11 +0200

    Time: 66 ms (81.15%), Space: 18 MB (19.67%) - LeetHub

--- 1616-split-two-strings-to-make-palindrome/1616-split-two-strings-to-make-palindrome.py
+++ 1616-split-two-strings-to-make-palindrome/1616-split-two-strings-to-make-palindrome.py
@@ -1,40 +1,36 @@
 class Solution:
-    def checkPalindromeFormation(self, a: str, b: str) -> bool:
-        ans= a==a[::-1] or b==b[::-1]
-        
-        start=len(a)//2-1
-        end=start+1+(len(a)&1)
-        a_is_palindrom=[True]+[False]*(len(a)//2-1)
-        for i in range(1,len(a)//2):
-            if a[start]==a[end]:
-                a_is_palindrom[i]=True
-            else:
+    def checkPalindromeFormation(self,a: str, b: str) -> bool:
+        ans = a == a[::-1] or b == b[::-1]
+        n=len(a)
+        index = -1
+        for i in range(n // 2):
+            if a[i] != b[~i]:
+                index = i
                 break
-            start-=1
-            end+=1
-                
-        b_is_palindrom=[True]+[False]*(len(b)//2-1)
-        start=len(b)//2-1
-        end=start+1+(len(b)&1)
-        for i in range(1,len(b)//2):
-            if b[start]==b[end]:
-                b_is_palindrom[i]=True
-            else:
+        if index!=-1:
+            s = a[index:n-index]
+            ans |= s == s[::-1]
+            s = b[index:n-index]
+            ans |= s == s[::-1]
+        else:
+            ans=True
+
+        index = -1
+        for i in range(n // 2):
+            if b[i] != a[~i]:
+                index = i
                 break
-            start-=1
-            end+=1
-                
-        for i in range(len(a)//2):
-            if a[i]!=b[~i]:
-                break
-            ans|=b_is_palindrom[~i]|a_is_palindrom[~i]
-        
-        for i in range(len(b)//2):
-            if b[i]!=a[~i]:
-                break
-            ans|=b_is_palindrom[~i]|a_is_palindrom[~i]
-                
+        if index!=-1:
+            s = a[index:n-index]
+            ans |= s == s[::-1]
+            s = b[index:n-index]
+            ans |= s == s[::-1]
+            print(s)
+        else:
+            ans=True
+
         return ans
+            
         
             
         
