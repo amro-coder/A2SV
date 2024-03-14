3f6cb72f307fdcbcaf1bba8cfd5b17516e59d4e7
Amro Salman <54499426+amro-coder@users.noreply.github.com>
2024-03-14 20:08:18 +0200

    Time: 114 ms (26.18%), Space: 20.2 MB (6.36%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,12 +1,43 @@
 class Solution:
-    def trap(self, height: List[int]) -> int:
-        max_right,left_max,ans=[0]*(len(height)+1),0,0
-        for i in range(len(height)-1,-1,-1):
-            max_right[i]=max(max_right[i+1],height[i])
-        for i in range(len(height)):
-            ans+=max(0,min(left_max,max_right[i+1])-height[i])
-            left_max=max(left_max,height[i])
+    def trap(self, arr: List[int]) -> int:
+        n = len(arr)
+
+        prefix_sum = [0]
+        acc = 0
+        for ele in arr:
+            acc += ele
+            prefix_sum.append(acc)
+
+
+        stack = []
+        mapper = {}
+        for i in range(n):
+
+            while stack:
+                value,idx = stack[-1]
+                if arr[i] >= value: # pop
+                    popped_value,popped_idx = stack.pop()
+                    mapper[popped_idx] = i
+                else:
+                    break
+            stack.append((arr[i],i))
+
+        for i in range(len(stack)-1):
+            _,curr_idx = stack[i]
+            _,next_idx = stack[i+1]
+            mapper[curr_idx] = next_idx
+        mapper[n-1] = None
+
+        ans = 0
+        curr_idx = 0
+        while mapper[curr_idx] is not None:
+            next_idx = mapper[curr_idx]
+            #
+            length = min(arr[curr_idx],arr[next_idx])
+            interval = next_idx-curr_idx-1
+            range_sum = prefix_sum[next_idx]-prefix_sum[curr_idx+1]
+            ans += length  * interval - range_sum
+
+            curr_idx = next_idx
+
         return ans
-            
-        
-        
