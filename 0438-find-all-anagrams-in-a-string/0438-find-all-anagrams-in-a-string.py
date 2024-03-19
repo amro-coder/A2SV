c5815b2f379138cec6890a7ad150ac980c5990f4
Amro Salman <54499426+amro-coder@users.noreply.github.com>
2024-03-19 16:52:27 +0200

    Time: 278 ms (25.86%), Space: 17.8 MB (34.65%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -1,25 +1,22 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        cnt, cnt_false, target_cnt, ans = defaultdict(int), 0, defaultdict(int), []
-        for value in p:
-            target_cnt[value] += 1
-        for i in range(min(len(p), len(s))):
-            cnt[s[i]] += 1
-        for key in set(list(target_cnt.keys())+list(cnt.keys())):
-            cnt_false += target_cnt[key] != cnt[key]
-        if cnt_false == 0:
+        ans,target_cnt,cnt=[],Counter(p),Counter(s[:min(len(p),len(s))])
+        if target_cnt==cnt:
             ans.append(0)
-        for i in range(len(p), len(s)):
-            cnt_false += (cnt[s[i - len(p)]] == target_cnt[s[i - len(p)]]) - (cnt[s[i - len(p)]] == target_cnt[s[i - len(p)]] + 1)
-            cnt[s[i - len(p)]] -= 1
-
-            cnt_false += (cnt[s[i]] == target_cnt[s[i]]) - (cnt[s[i]] == target_cnt[s[i]] - 1)
-            cnt[s[i]] += 1
-
-            if cnt_false == 0:
-                ans.append(i - len(p) + 1)
-        return ans
+        for i in range(len(p),len(s)):
+            cnt[s[i]]+=1
+            cnt[s[i-len(p)]]-=1
+            if cnt[s[i]]==0:
+                del cnt[s[i]]
+            if cnt[s[i-len(p)]]==0:
+                del cnt[s[i-len(p)]]
             
+            if target_cnt==cnt:
+                ans.append(i-len(p)+1)
+                
+        return ans
+                
+        
         
         
             
