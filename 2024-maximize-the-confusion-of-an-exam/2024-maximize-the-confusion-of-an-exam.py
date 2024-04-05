d3f2227bfcd8a859660772c4525aa362dc695494
Amro Salman <54499426+amro-coder@users.noreply.github.com>
2024-04-05 20:11:40 +0200

    Time: 505 ms (5.86%), Space: 16.8 MB (87.09%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.py
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.py
@@ -1,21 +1,17 @@
 class Solution:
     def maxConsecutiveAnswers(self, answerKey: str, k: int) -> int:
-        cnt,j,ans=0,-1,0
+        cnt1,j1,cnt2,j2,ans=0,-1,0,-1,0
         for i in range(len(answerKey)):
-            while j+1<len(answerKey) and (cnt + (answerKey[j+1]=="F"))<=k:
-                cnt += (answerKey[j+1]=="F")
-                j+=1
-            ans=max(ans,j-i+1)
-            cnt -= answerKey[i]=="F"
-            
-        cnt,j=0,-1
-        for i in range(len(answerKey)):
-            while j+1<len(answerKey) and (cnt + (answerKey[j+1]=="T"))<=k:
-                cnt += (answerKey[j+1]=="T")
-                j+=1
-            ans=max(ans,j-i+1)
-            cnt -= answerKey[i]=="T"
-        
+            while j1+1<len(answerKey) and (cnt1 + (answerKey[j1+1]=="F"))<=k:
+                cnt1 += (answerKey[j1+1]=="F")
+                j1+=1
+            while j2+1<len(answerKey) and (cnt2 + (answerKey[j2+1]=="T"))<=k:
+                cnt2 += (answerKey[j2+1]=="T")
+                j2+=1
+            ans=max(ans,max(j1,j2)-i+1)
+            cnt1 -= answerKey[i]=="F"
+            cnt2 -= answerKey[i]=="T"
+                
             
         return ans
                 
