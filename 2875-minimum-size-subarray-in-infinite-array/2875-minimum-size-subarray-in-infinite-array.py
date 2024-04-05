78c76e1c74483a4d9a3b59fa5f9f45f3daada761
Amro Salman <54499426+amro-coder@users.noreply.github.com>
2024-04-05 18:58:15 +0200

    Time: 729 ms (5.50%), Space: 65.9 MB (7.11%) - LeetHub

---
+++ 2875-minimum-size-subarray-in-infinite-array/2875-minimum-size-subarray-in-infinite-array.py
@@ -0,0 +1,25 @@
+class Solution:
+    def minSizeSubarray(self, nums: List[int], target: int) -> int:
+        ans=float("inf")
+        extended_nums=nums+nums
+        prefix_nums=[0]+list(accumulate(nums))
+        last_index=defaultdict(lambda :-1)
+        for i in range(len(nums)+1):
+            if last_index[prefix_nums[i]-target]!=-1:
+                ans=min(ans,i-last_index[prefix_nums[i]-target])
+            last_index[prefix_nums[i]]=i
+        
+        c=sum(nums)
+        rem=target%c
+        prefix_ex_nums=[0]+list(accumulate(extended_nums))
+        last_index=defaultdict(lambda :-1)
+        for i in range(len(extended_nums)+1):
+            if last_index[(prefix_ex_nums[i]%c-rem)%c]!=-1:
+                temp_sum=prefix_ex_nums[i]-prefix_ex_nums[last_index[(prefix_ex_nums[i]%c-rem)%c]]
+                ans=min(ans,i-last_index[(prefix_ex_nums[i]%c-rem)%c] + ((target-temp_sum)//c)*len(nums))
+            if last_index[prefix_ex_nums[i]%c]==-1 or prefix_ex_nums[i]>=prefix_ex_nums[last_index[prefix_ex_nums[i]%c]]:
+                last_index[prefix_ex_nums[i]%c]=i
+        
+        return ans if ans!=float("inf") else -1
+    
+        
