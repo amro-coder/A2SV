12bd0bce7e8e1d3d4ccabfd86a968d096d4db318
Amro Salman <54499426+amro-coder@users.noreply.github.com>
2024-04-26 14:38:04 +0300

    Time: 480 ms (90.48%), Space: 37.9 MB (9.57%) - LeetHub

--- 0456-132-pattern/0456-132-pattern.py
+++ 0456-132-pattern/0456-132-pattern.py
@@ -1,20 +1,11 @@
 class Solution:
     def find132pattern(self, nums: List[int]) -> bool:
-        n=len(nums)
-        prevBigger=[-1]*n
-        stack=[]
-        for i in range(n-1,-1,-1):
-            while stack and nums[i]>nums[stack[-1]]:
-                prevBigger[stack.pop()]=i
-            stack.append(i)
-        stack=[]
-        for i in range(n):
-            stack.append((nums[i],prevBigger[i]))
-        stack.sort()
-        for i in range(n):
-            while stack and stack[-1][1]<=i:
-                stack.pop()
-            if stack and stack[-1][0]>nums[i]:
+        stack,biggestValidNum=[],float("-inf")
+        for i in range(len(nums)-1,-1,-1):
+            if nums[i]<biggestValidNum:
                 return True
+            while stack and nums[i]>stack[-1]:
+                biggestValidNum=stack.pop()
+            stack.append(nums[i])
         return False
         
