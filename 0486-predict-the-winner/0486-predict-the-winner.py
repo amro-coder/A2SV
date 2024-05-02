48aac823109f14823c20f1006d92a3668cdd3a94
Amro Salman <54499426+amro-coder@users.noreply.github.com>
2024-05-02 13:05:19 +0300

    Time: 8377 ms (5.02%), Space: 16.6 MB (39.29%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -1,23 +1,21 @@
 class Solution:
     def predictTheWinner(self, nums: List[int]) -> bool:
-        n=len(nums)
-        dp=[[0]*n for _ in range(n)]
-        prefixSum=[0]+list(accumulate(nums))
-        for i in range(n):
-            dp[i][i]=nums[i]
-        for length in range(2,n+1):
-            for i in range(n-length+1):
-                j=i+length-1
-                dp[i][j]=max(nums[i]+ prefixSum[j+1]-prefixSum[i+1]-dp[i+1][j],nums[j]+prefixSum[j]-prefixSum[i]-dp[i][j-1])
-        return 2*dp[0][n-1]>=sum(nums)
-#         def getMaxScore(nums,ind=0):
-#             if not nums:
-#                 return [0,0]
-#             op1=getMaxScore(nums[1:],1^ind)
-#             op2=getMaxScore(nums[:-1],1^ind)
-#             op1[ind]+=nums[0]
-#             op2[ind]+=nums[-1]
-#             return max(op1,op2,key=lambda a:a[ind])
+        def getMaxScore(nums,myturn=True):
+            if not nums:
+                return (0,0)
+            if myturn:
+                op1=getMaxScore(nums[1:],False)
+                op1=(op1[0]+nums[0],op1[1])
+                op2=getMaxScore(nums[:-1],False)
+                op2=(op2[0]+nums[-1],op2[1])
+                return max(op1,op2)
                 
-#         player1,player2=getMaxScore(nums)
-#         return player1>=player2
+            else:
+                op1=getMaxScore(nums[1:],True)
+                op1=(op1[0],op1[1]+nums[0])
+                op2=getMaxScore(nums[:-1],True)
+                op2=(op2[0],op2[1]+nums[-1])
+                return min(op1,op2)
+                
+        player1,player2=getMaxScore(nums)
+        return player1>=player2
