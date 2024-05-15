ac4263211c7dfb1402c891e6a384e70a09011329
Amro Salman <54499426+amro-coder@users.noreply.github.com>
2024-05-15 13:41:02 +0300

    Time: 52 ms (18.42%), Space: 19.8 MB (10.02%) - LeetHub

---
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
+        dpMax={}
+        dpMax[None]=0
+        def getMaxDepth(node):
+            if node==None: return 0
+            if node in dpMax:
+                return dpMax[node]
+            dpMax[node]=max(getMaxDepth(node.left),getMaxDepth(node.right))+1
+            return dpMax[node]
+        getMaxDepth(root)
+        def solve(node):
+            if dpMax[node]==1:
+                return node
+            if dpMax[node.left]>=dpMax[node.right]:
+                return solve(node.left)
+            return solve(node.right)
+        return solve(root).val
