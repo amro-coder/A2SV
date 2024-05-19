f9697e44469e8e3b959d418d5f324c3d9bbfde02
Amro Salman <54499426+amro-coder@users.noreply.github.com>
2024-05-19 22:10:32 +0300

    Time: 50 ms (87.74%), Space: 19.9 MB (65.63%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
@@ -6,53 +6,22 @@
 #         self.right = right
 class Solution:
     def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
-        node,parent=root,None
-        while node and node.val!=key:
-            parent=node
-            if key<node.val:
-                node=node.left
+        def get_min(r):
+            while r.left: r=r.left
+            return r.val
+        def delete(r,key):# takes the tree and the key. returns the tree after deleting key
+            if not r: return None
+            if key<r.val: r.left=delete(r.left,key)
+            elif key>r.val: r.right=delete(r.right,key)
             else:
-                node=node.right
-                
-        if not node: return root
-
-        if not node.left:
-            if not parent: return node.right
-            if parent.left==node: parent.left=node.right
-            else: parent.right=node.right
-            return root
-        
-        if not node.right:
-            if not parent: return node.left
-            if parent.left==node: parent.left=node.left
-            else: parent.right=node.left
-            return root
-        
-        parentReplace=None
-        replaceNode=node.right
-        while replaceNode.left:
-            parentReplace=replaceNode
-            replaceNode=replaceNode.left
-        
-        if parentReplace==None:
-            replaceNode.left=node.left
-            if not parent: return replaceNode
-            if parent.left==node: parent.left=replaceNode
-            else: parent.right=replaceNode
-            return root            
-        else:
-            edgeReplaceNode=replaceNode
-            while edgeReplaceNode.right:
-                edgeReplaceNode=edgeReplaceNode.right
-                
-            edgeReplaceNode.right=node.right
-            replaceNode.left=node.left
-            parentReplace.left=None
-            if not parent:
-                return replaceNode
-            if parent.left==node: parent.left=replaceNode
-            else: parent.right=replaceNode
-            return root
+                if not r.left: return r.right
+                if not r.right: return r.left
+                r.val=get_min(r.right)
+                r.right=delete(r.right,r.val)
+            return r
+        return delete(root,key)
+            
+            
             
             
         
