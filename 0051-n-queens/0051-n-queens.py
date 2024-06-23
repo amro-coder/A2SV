34d3077bfe3cf774fa49e65b29a9677deabbdb4d
Amro Salman <54499426+amro-coder@users.noreply.github.com>
2024-06-23 18:19:47 +0300

    Time: 108 ms (10.96%), Space: 17 MB (62.22%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -1,45 +1,20 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        
-        
-        ans = []
-        cur = []
-        
+        ans,cur=[],[]
         def backtrack():
-            nonlocal n
-            if len(cur) == n:
-                temp = []
-                for j in cur:
-                    temp.append('.' * j + 'Q' + '.' * (n - j - 1))
-                ans.append(temp.copy())
+            r=len(cur)
+            if r==n:
+                ans.append(['.'*cur[i]+'Q'+'.'*(n-cur[i]-1) for i in range(n)])
                 return
-            
-            r = len(cur)
             for c in range(n):
-                for i in range(len(cur)):
-                    j = cur[i]
-                    if c == j or abs(c - j) == (r - i):
-                        break
-                else:
+                if not any([abs(i-r)==abs(cur[i]-c) or cur[i]==c for i in range(r)]):
                     cur.append(c)
                     backtrack()
                     cur.pop()
-        
         backtrack()
         return ans
+    
+                        
+                    
+            
         
-        
-#         [1, 3, 0, 2]
-#         ['.Q..', '...Q']
-#         [1, 3, j]
-        
-#         i, j = 0, 3
-#         x, y = 2, 1
-
-
-# 0, 1
-# 1, 2
-# 2, 3
-
-# 0, 3
-# 1, 2
