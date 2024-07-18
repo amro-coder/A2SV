4d6a41afd140f9e7ee968c18e759b272ac9c4fd7
Amro Salman <54499426+amro-coder@users.noreply.github.com>
2024-07-18 23:08:15 +0300

    Time: 389 ms (99.33%), Space: 31.3 MB (44.97%) - LeetHub

--- 2366-minimum-replacements-to-sort-the-array/2366-minimum-replacements-to-sort-the-array.py
+++ 2366-minimum-replacements-to-sort-the-array/2366-minimum-replacements-to-sort-the-array.py
@@ -5,14 +5,10 @@ class Solution:
         for i in range(len(nums)-2,-1,-1):
             if nums[i]<=maxVal:
                 maxVal=nums[i]
-            elif nums[i]<=2*maxVal:
-                ans+=1
-                maxVal=nums[i]//2
             else:
                 numOp=(nums[i]-1)//maxVal
                 ans+=numOp
                 maxVal=nums[i]//(numOp+1)
-
         return ans
             
         
