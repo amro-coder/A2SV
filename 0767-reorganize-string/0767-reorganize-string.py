dca0971f7580f3fef161db49a8b7fc294ed34a45
Amro Salman <54499426+amro-coder@users.noreply.github.com>
2024-07-26 20:51:09 +0300

    Time: 40 ms (35.77%), Space: 16.6 MB (22.42%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.py
+++ 0767-reorganize-string/0767-reorganize-string.py
@@ -5,23 +5,18 @@ class Solution:
         rest_freq=sum([i[0] for i in x[:-1]])
         if max_freq-rest_freq>1:
             return ""
-        ans=[]
-        while x:
-            if not ans or ans[-1]!=x[-1][1]:
-                ans.append(x[-1][1])
-                if x[-1][0]==1:
-                    x.pop()
-                else:
-                    x[-1][0]-=1
-            else:
-                ans.append(x[-2][1])
-                if x[-2][0]==1:
-                    temp=x.pop()
-                    x.pop()
-                    x.append(temp)
-                else:
-                    x[-2][0]-=1
-            x.sort()
+        ans=['']*(len(s))
+        for i in range(0,len(s),2):
+            ans[i]=x[-1][1]
+            x[-1][0]-=1
+            if x[-1][0]==0:
+                x.pop()
+        for i in range(1,len(s),2):
+            ans[i]=x[-1][1]
+            x[-1][0]-=1
+            if x[-1][0]==0:
+                x.pop()
+            
         return "".join(ans)
         
         
