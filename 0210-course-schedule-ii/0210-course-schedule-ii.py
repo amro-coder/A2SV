6b4e532c306389af93a07f77242d930e2c12e6c9
Amro Salman <54499426+amro-coder@users.noreply.github.com>
2024-07-30 22:17:10 +0300

    Time: 87 ms (75.96%), Space: 18.3 MB (56.19%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -15,6 +15,7 @@ class Solution:
             vis[node]=2
             ans.append(node)
             return cycle
+        
         vis=[0]*n# 0=not vis, 1=under processing, 2=visited.
         ans=[]
         cycle=False
