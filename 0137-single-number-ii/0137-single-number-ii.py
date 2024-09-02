62565ebd719ec8239239cde7b1d52b45ea9aff60
Amro Salman <54499426+amro-coder@users.noreply.github.com>
2024-09-02 17:14:39 +0300

    Time: 43 ms (98.70%), Space: 18.6 MB (76.43%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -1,17 +1,7 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        cnt=[0]*33
+        ones=twos=0
         for val in nums:
-            for i in range(33):
-                cnt[i]+=(val>>i)&1
-        ans=0
-        if cnt[32]%3==1:
-            for i in range(33):
-                if cnt[i]%3==0:
-                    ans-=1<<i
-            ans-=1
-        else:
-            for i in range(33):
-                if cnt[i]%3:
-                    ans+=1<<i
-        return ans
+            ones=(ones^val) & ~twos
+            twos=(twos^val) & ~ones
+        return ones
