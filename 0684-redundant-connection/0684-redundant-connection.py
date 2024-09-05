0281ec303271bbe62dc2e1b557072c9878f70b01
Amro Salman <54499426+amro-coder@users.noreply.github.com>
2024-09-05 19:35:10 +0300

    Time: 51 ms (80.99%), Space: 17.1 MB (61.31%) - LeetHub

---
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -0,0 +1,25 @@
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        
+        def find_parent(x):
+            while(x!=parent[x]):
+                x=parent[x]
+            return x
+
+        def unite(x,y):
+            parent_x,parent_y=find_parent(x),find_parent(y)
+            if parent_x!=parent_y:
+                if size[parent_x]<size[parent_y]:
+                    parent_x,parent_y=parent_y,parent_x
+                size[parent_x]+=size[parent_y]
+                parent[parent_y]=parent_x
+                return True
+            return False
+        
+        n=max([max(i) for i in edges])
+        parent=[i for i in range(n)]
+        size=[1]*n
+        for u,v in edges:
+            if not unite(u-1,v-1):
+                return (u,v)
+        
